77b91e255a9c44148665f84d1498a93104df8384
bryanb213 <blancasbryan@gmail.com>
2020-06-30 15:23:13 -0500

    messed with Math and strings

--- BackToBasics/Program.cs
+++ BackToBasics/Program.cs
@@ -1,13 +1,118 @@
 ﻿using System;
 
+
 namespace BackToBasics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //test
+            //CONVERT
+
+            //int x = 2;
+            //string y = "";
+            //float c = 5.9f;
+
+            //y = x.ToString();
+            //Console.WriteLine(y);
+
+            //x = Convert.ToInt32(y);
+
+            //Console.WriteLine(x);
+            //x = (int)c;
+            //Console.WriteLine(x);
+
+
+            //DATE TIME
+
+            //DateTime date = DateTime.Now;
+            //string format = string.Format("formatting date is, {0:yy-MM-dd}",date);
+
+            //EXERCISE
+
+            //DateTime ex1 = DateTime.Now;
+
+            //string format1 = string.Format("first Format: {0:dd-MM-yyyy hh:mm:ss}", ex1);
+
+            //string format2 = string.Format("Secong format, {0} of month {1:MMMM} year {2}", ex1.DayOfWeek, ex1, ex1.Year);
+            //string formatfix2 = string.Format("{0:dddd 'of month' MMMM 'year' yyyy}", ex1);
+
+            //string format3 = string.Format("third format, \n Day {0} \n Month {1:MMMM} \n Year {2}", ex1.DayOfWeek, ex1, ex1.Year);
+            //string formatfix3 = string.Format("Day {0:dddd \n 'Month' MMMM \n 'Year' yyyy}",ex1);
+
+            //Console.WriteLine(format1);
+            //Console.WriteLine(formatfix2);
+            //Console.WriteLine(formatfix3);
+
+
+            //STRING MANIPULATION
+
+            //string movie = "    Shrek the 3rd";
+
+            //Indexes
+            //Console.WriteLine(movie[1]);
+
+            //Trim
+            //removes useless whitespace
+            //Console.WriteLine(movie.Trim());
+
+
+            //uppercase
+            //Console.WriteLine(movie.ToUpper().Trim());
+
+            //Replace
+            //Console.WriteLine(movie.Replace('k','H'));
+
+            //Substring
+            //Console.WriteLine(movie.Substring(7));
+
+            //Remove
+            //Console.WriteLine(movie.Remove(5));
+
+            //Insert
+            //Console.WriteLine(movie.Trim().Insert(7, "chicken"));
+            //Console.WriteLine(movie + "\a");
+
+            //Math
+            //float x = 3.1412f;
+
+            //Ceiling
+            //Console.WriteLine("Ceiling: " + Math.Ceiling(x));
+
+
+            //floor
+            //Console.WriteLine("Floor: " + Math.Floor(x));
+
+            //round
+            //Console.WriteLine("round: " + Math.Round(x));
+            //by decimal
+            //Console.WriteLine("round by decimal: " + Math.Round(x,2));
+
+            //truncate
+            //Console.WriteLine("truncate: " + Math.Truncate(x));
+
+            //int p = 5;
+            //int u = 6;
+
+            //Max
+            //Console.WriteLine(Math.Max(p, u));
+
+            //pow
+            //Console.WriteLine("Powaaaa: " + Math.Pow(x,2));
+            //Console.WriteLine("Powaaaa: " + Math.Pow(2, 2));
+
+            //RANDOM
+
+            //Random rand = new Random();
+            //int x = 0;
+            //x = rand.Next(1,5); //from 1 to 4
+            //Console.WriteLine(x);
+
+
+
+            //ARRAYS
+
+
         }
     }
 }
